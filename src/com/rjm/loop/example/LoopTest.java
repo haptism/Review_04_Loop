ba8415d0f9656d379316cc4f94abe9e1ffd79c51
{haptism} <haptism007@gmail.com>
2019-04-02 17:40:34 +0900

    Initial commit

---
+++ src/com/rjm/loop/example/LoopTest.java
@@ -0,0 +1,7 @@
+package com.rjm.loop.example;
+
+public class LoopTest {
+	public void printStar1(){}
+	public void printStar2(){}
+
+}
---
+++ src/com/rjm/loop/example/LoopTestMain.java
@@ -0,0 +1,10 @@
+package com.rjm.loop.example;
+
+public class LoopTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
